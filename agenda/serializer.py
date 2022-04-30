24c65b838955ebf6cbd11dee486a00ecc0e8028d
Sérgio Roberto Sanchez Filho <ssanchezfilho@gmail.com>
2022-04-30 14:47:30 -0300

    validations of field and of objects level
    Definindo algumas validações de campo e Objects Levels para nossa aplicação, como por exemplo, não pode fazer agendamento  no passado, e email brasileiro associado a número de telefone brasileiro.

--- agenda/serializer.py
+++ agenda/serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from agenda.models import Agendamento
+from django.utils import timezone
+
 
 class AgendamentoSerializer(serializers.Serializer):
     data_horario = serializers.DateTimeField()
@@ -7,6 +9,19 @@ class AgendamentoSerializer(serializers.Serializer):
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=25)
 
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado.")
+        return value
+    
+    def validate(self, attrs):
+        telefone_cliente = attrs.get("telefone_cliente", "")
+        email_cliente = attrs.get("email_cliente", "")
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+            raise serializers.ValidationError("Email brasileiro deve estar associado a um número do Brasil (+55).")
+        return attrs
+    
     def create(self, validated_data):
         agendamento = Agendamento.objects.create(
             data_horario=validated_data["data_horario"],
